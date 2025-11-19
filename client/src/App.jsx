b36b5e128acecc18896a123025321d122744308c
kirtannn1 <119801478+notkirtann@users.noreply.github.com>
2025-11-19 17:44:44 +0530

    0.1.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,12 @@ import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
-import Card from './pages/Cart'
 import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -27,6 +27,7 @@ const App = () => {
         <Route path='/place-order' element={<PlaceOrder/>}/>
         <Route path='/orders' element={<Orders/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
