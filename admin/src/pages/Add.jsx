2a483e99c6d3f72bd3bd0026e5bf68273236fa43
kirtannn1 <119801478+notkirtann@users.noreply.github.com>
2025-11-24 12:51:03 +0530

    2.0.3

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -152,7 +152,7 @@ const Add = ({ token }) => {
         <label className='cursor-pointer' htmlFor="bestseller">Add to Bestseller</label>
       </div>
 
-      <button type="submit" className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
+      <button type="submit" className='w-28 py-3 mt-4 bg-black text-white cursor-pointer'>ADD</button>
 
     </form>
   )
