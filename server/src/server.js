8ab39972ad60bed89a69ee44bad5e9471967e5f3
kirtannn1 <119801478+notkirtann@users.noreply.github.com>
2025-11-26 15:06:43 +0530

    2.5.9

--- server/src/server.js
+++ server/src/server.js
@@ -16,14 +16,17 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['https://i-own-jersey.vercel.app',
-  'https://i-own-jersey-admin.vercel.app',
-  'http://localhost:5173',
-  'http://localhost:5174'],
+  origin: [
+    "https://i-own-jersey.vercel.app",
+    "https://i-own-jersey-admin.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:5174"
+  ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-   allowedHeaders: ["Content-Type", "Authorization", "token"],
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "*",
 }));
+
 
 app.get('/', (req, res) => {
   res.json({ success: true, message: 'API is working!' });
