eb70c9a851733b7b957e079cc842a8cac55f57af
kirtannn1 <119801478+notkirtann@users.noreply.github.com>
2025-11-26 14:56:37 +0530

    2.5.8

--- server/src/server.js
+++ server/src/server.js
@@ -22,7 +22,7 @@ app.use(cors({
   'http://localhost:5174'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+   allowedHeaders: ["Content-Type", "Authorization", "token"],
 }));
 
 app.get('/', (req, res) => {
